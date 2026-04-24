b5942363034d7f9885c19b66d6ad76dac6fd517b
HugoFranco <hugopereirafranco47@gmail.com>
2026-04-23 21:40:53 -0300

    se trae informacion de la API apartir de la author_key

--- postgres/schema.sql
+++ postgres/schema.sql
@@ -1,1 +1,37 @@
--- Active: 1775580324086@@127.0.0.1@5432@ConsultaMortal
+DROP TABLE IF EXISTS author,author_book,category,books CASCADE;
+
+CREATE TABLE author (
+    id_author SERIAL PRIMARY KEY,
+    author_name VARCHAR(100),
+    birth_year SMALLINT,
+    country VARCHAR(100),
+    external_api_id VARCHAR(100),
+    total_known_works INTEGER,
+    api_source VARCHAR(100)
+);
+
+CREATE TABLE category (
+    id_category INTEGER PRIMARY KEY,
+    category_name VARCHAR(80)
+);
+
+CREATE TABLE books (
+    id_book SERIAL PRIMARY KEY,
+    id_category INTEGER NOT NULL,
+    title VARCHAR(100),
+    price NUMERIC(12,2),
+    rating SMALLINT,
+    CONSTRAINT fk_category FOREIGN KEY (id_category)
+        REFERENCES category(id_category)
+);
+
+CREATE TABLE author_book(
+    id_author_book SERIAL PRIMARY KEY,
+    id_author INTEGER NOT NULL,
+    id_book INTEGER NOT NULL,
+    CONSTRAINT fk_author FOREIGN KEY (id_author)
+        REFERENCES author(id_author),
+    CONSTRAINT fk_book FOREIGN KEY (id_book)
+        REFERENCES books(id_book)
+);
+
